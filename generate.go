01db9983f8d7be648a2b323e4c0f46669d25bed8
Michal Hruby <michal.mhr@gmail.com>
2024-10-19 20:43:45 +0100

    regen code

---
+++ generate.go
@@ -0,0 +1,5 @@
+//go:generate go run github.com/mhr3/gocc/cmd/gocc@v0.11.0 csrc/adler32_neon.c -l -p adler32 -o ./ -a arm64 -O3
+//go:generate go run github.com/mhr3/gocc/cmd/gocc@v0.11.0 csrc/adler32_sse3.c -l -p adler32 -o ./ -a amd64 -m ssse3 -O3
+//go:generate go run github.com/mhr3/gocc/cmd/gocc@v0.11.0 csrc/adler32_avx2.c -l -p adler32 -o ./ -a avx2 -O3
+
+package adler32
